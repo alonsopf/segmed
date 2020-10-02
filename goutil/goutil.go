dc820579420c2c1f2ddade864e02aa126bbb7898
alonsopf <alonsopf@hotmail.com>
2020-10-02 11:41:06 -0500

    add search word

--- goutil/goutil.go
+++ goutil/goutil.go
@@ -2,10 +2,28 @@ package goutil
 import (
 	"crypto/sha512"
 	"encoding/base64"
+	"golang.org/x/oauth2"
+	unsplash "github.com/hbagdi/go-unsplash/unsplash"
+	config "github.com/alonsopf/segmed/config"
 )
 
 func ToSha512(str []byte) string {
 	hasher := sha512.New()
     hasher.Write(str)
 	return base64.StdEncoding.EncodeToString(hasher.Sum(nil))
+}
+
+func SearchPhotosByWord(word string) (*unsplash.PhotoSearchResult, error) {
+	configuration := config.GetConfig("prod")
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: configuration.UNSPLASH_ACCESS_KEY},
+	)
+	client := oauth2.NewClient(oauth2.NoContext, ts)
+	unsclient := unsplash.New(client)  
+	searchOpt := &unsplash.SearchOpt {Query : word}
+	photos, _, err := unsclient.Search.Photos(searchOpt)
+	if err != nil {
+	  return nil, err
+	}
+	return photos, nil
 }
--- goutil/goutil_test.go
+++ goutil/goutil_test.go
@@ -11,6 +11,6 @@ func TestSHA512(t *testing.T) {
 	expected := "1qG05wqBwhyjE45d66cnZwYHrUD+MVEBOe2sFDtZoAdXGls0g6xchvQsd2Um5dok8fmF58X4khqvQDP+3ZwIsw=="
 	got := IsSHA512("segmedai")
 	if got != expected {
-		t.Errorf("Expected: %v, got: %v", expected, got)
+		t.Errorf("sha512 Expected: %v, got: %v", expected, got)
 	}
 }
