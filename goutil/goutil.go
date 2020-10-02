24120a9cb35c903eb0f23c976e68696066a9403a
alonsopf <alonsopf@hotmail.com>
2020-10-02 16:36:07 -0500

    remove fmt

--- goutil/goutil.go
+++ goutil/goutil.go
@@ -6,7 +6,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
-	"fmt"
 	config "github.com/alonsopf/segmed/config"
 )
 
@@ -98,7 +97,6 @@ type Photos struct {
 func SearchPhotosByWord(word string) (*map[int]*Photos, error, int , int) {
 	configuration := config.GetConfig("prod")
 	view := "https://api.unsplash.com/search/photos/?query="+word+"&client_id="+configuration.UNSPLASH_ACCESS_KEY
-	fmt.Println(view)
 	req, err := http.NewRequest("GET", view , nil)
 	if err != nil {
 		return nil, err, 0, 0
@@ -113,12 +111,16 @@ func SearchPhotosByWord(word string) (*map[int]*Photos, error, int , int) {
 	buf2.ReadFrom(resp.Body)
 	defer resp.Body.Close()
 	s2 := buf2.String()
-	fmt.Println(s2)
 	json.Unmarshal([]byte(s2), &result)
 	PhotosList := make(map[int]*Photos)
 	count := 0
+	var desc string
 	for _, photo := range result.Results {
-		PhotosList[count] = &Photos{photo.ID, photo.Urls.Regular, photo.Description.(string), photo.AltDescription, strconv.Itoa(photo.Likes), photo.User.Name}
+		desc = ""
+		if photo.Description != nil {
+			desc = photo.Description.(string)
+		}
+		PhotosList[count] = &Photos{photo.ID, photo.Urls.Regular, desc, photo.AltDescription, strconv.Itoa(photo.Likes), photo.User.Name}
         count++
     }
 	return &PhotosList, nil, result.Total, result.TotalPages
